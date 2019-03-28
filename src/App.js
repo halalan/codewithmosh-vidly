1fe4cffc1cdfac2caf604d7ff600e453790976c7
halalan <halalanrizkon52@gmail.com>
2019-03-28 13:39:31 +0700

    final Conditional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import Movies from './component/Conditional/movies';
 
-import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
     <main className="container">
-    <h1>Hello word</h1>
+    <Movies />
     </main>
+ 
+  
+    </div>
     );
   }
 }
